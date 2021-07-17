0f421d72783e2b4ac69b04f5e14fbf370ccad8e1
Carsten Schmidt <jazzschmidt@icloud.com>
2021-07-17 19:52:35 +0200

    Sorts parameters in output

--- steroid.sh
+++ steroid.sh
@@ -13,6 +13,19 @@ declare -r app_description="My custom app"
 function setup() {
   add_flag "name string" "n" "assigns a name"
   add_flag "queue url" "Adds a url"
+
+  # TODO:
+  flag "q" "quiet" "silences all messages"; q=$flag
+  flag "debug" "verbose output"
+  flag "p" "printer friendly output"
+
+  arg "n" "name" "text" "sets the name to <text>" # -n, --name text - sets the name to <text>
+  arg "b" "base" "base key for encryption" #        -b         base - base key for encryption
+  arg "mad-hatter" "name" "name of the mad hatter" # --mad-hatter name - name of the mad hatter
+  arg "g" "routine" "grant routine spec" #          -g routine      - grant routine spec
+
+  print_args
+  exit 0
 }
 
 function teardown() {
@@ -55,6 +68,10 @@ function main() {
   else
     echo "Unknown"
   fi
+}
+
+function error() {
+    >&2 printf "%s %s: %s\n" "$app_name" "${FUNCNAME[1]}" "$*" && exit 1
 }
 
 function print_usage() {
@@ -120,6 +137,164 @@ function add_cmd() {
   local name=$1; local func=$2; local description=$3
   __commands+=("${name}:${description}")
   __commands_f+=("${func}")
+}
+
+{
+  # Flags
+
+  flag=
+  __flags_list=()
+
+  # flag "q" "qiet" "silences all messages"
+  # flag "debug" "verbose output"
+  # flag "p" "printer friendly output"
+  # $short#$long#$description
+  function flag() {
+    local short; local long; local desc;
+
+    if [ "$#" -ne 2 ] && [ "$#" -ne 3 ]; then
+      error "wrong number of arguments"
+    fi
+
+    while [ $# -gt 1 ]; do
+      local value="$1"
+      if [ "${#value}" -eq 1 ]; then
+        short="${value}"
+      else
+        long="${value}"
+      fi
+      shift
+    done
+
+    desc="$1"
+
+    $__setupFinished && scope="local" || scope="global"
+
+    __flags_list+=("${scope}#${short:--}#${long:--}#${desc}")
+
+    flag=false && has_flag "$short" "$long"
+  }
+
+  function has_flag() {
+    local short="${1:-#}"; local long="${2:-#}";
+
+    for arg in "${__args[@]}"; do
+      if [ "$arg" == "-$short" ] || [ "$arg" == "--$long" ]; then
+        flag=true && return 0
+      fi
+    done
+
+    return 1
+  }
+}
+
+{
+  # Args
+
+  arg=
+  __args_list=()
+
+  # arg "n" "name" "text" "sets the name to <text>" # -n, --name text - sets the name to <text>
+  # arg "b" "base" "base key for encryption" #        -b         base - base key for encryption
+  # arg "mad-hatter" "name" "name of the mad hatter" # --mad-hatter name - name of the mad hatter
+  # arg "g" "routine" "grant routine spec" #          -g routine      - grant routine spec
+  function arg() {
+    local short; local long; local param; local desc;
+
+    if [ "$#" -ne 3 ] && [ "$#" -ne 4 ]; then
+      error "wrong number of arguments"
+    fi
+
+    args=( "$@" )
+    for i in $(seq "$#" 1); do
+      value="${args[$i-1]}"
+
+      if [ -z "$desc" ]; then
+        desc="$value"
+      elif [ -z "$param" ]; then
+        param="$value"
+      elif [ "${#value}" -eq 1 ]; then
+        short="$value"
+      else
+        long="$value"
+      fi
+    done
+
+    $__setupFinished && scope="local" || scope="global"
+
+    __args_list+=("${scope}#${short:--}#${long:--}#${param}#${desc}")
+    arg="" && get_arg "$short" "$long"
+  }
+
+  function get_arg() {
+    local short="${1:-#}"; local long="${2:-#}"; local match=false;
+
+    for arg_v in "${__args[@]}"; do
+      if $match; then
+        arg="$arg_v" && return 0
+      fi
+
+      if [ "$arg_v" == "-$short" ] || [ "$arg_v" == "--$long" ]; then
+        match=true
+      fi
+    done
+
+    return 1
+  }
+
+  function print_args() {
+    local options_local=(); local options_global=();
+    local desc_local=(); local desc_global=();
+    local intend_local=0; local intend_global=0;
+
+    local is_args=true
+    for arg in "${__args_list[@]}" "###" "${__flags_list[@]}"; do
+      if [ "$arg" = "###" ]; then
+        is_args=false
+        continue
+      fi
+
+      scope=$(echo "$arg" | cut -d'#' -f1)
+      short=$(echo "$arg" | cut -d'#' -f2)
+      long=$(echo "$arg" | cut -d'#' -f3)
+
+      if $is_args; then
+        param=$(echo "$arg" | cut -d'#' -f4)
+        desc=$(echo "$arg" | cut -d'#' -f5-)
+      else
+        desc=$(echo "$arg" | cut -d'#' -f4-)
+      fi
+
+      # Remove dash (-)
+      short="${short%-}"; long="${long%-}"
+
+      line=
+
+      if [ -n "$short" ] && [ -n "$long" ]; then line="-$short, --$long"; fi;
+      if [ -n "$short" ] && [ -z "$long" ]; then line="-$short"; fi;
+      if [ -z "$short" ] && [ -n "$long" ]; then line="--$long"; fi;
+      if $is_args; then line+=" ${param}"; fi;
+
+      if [ "$scope" = "local" ]; then
+        options_local+=("$line")
+        desc_local+=("$desc")
+        if [ ${#line} -gt "$intend_local" ]; then intend_local=${#line}; fi;
+      else
+        options_global+=("$line")
+        desc_global+=("$desc")
+        if [ ${#line} -gt "$intend_global" ]; then intend_global=${#line}; fi;
+      fi
+    done
+
+    global_out=""; i=0
+    for option in "${options_global[@]}"; do
+      global_out+=$(printf "  %-${intend_global}s   %s" "$option" "${desc_global[$i]}")
+      global_out+="\n"
+      (( i++ ))
+    done
+
+    echo -en "${global_out}" | sort -d
+  }
 }
 
 __flags=() # Global flags
