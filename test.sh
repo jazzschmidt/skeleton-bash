c1cb52a6bd42895e35aeb4d4280330ccd4ed50fd
Carsten Schmidt <jazzschmidt@icloud.com>
2021-07-20 19:45:01 +0200

    Renames variables according to scope

--- test.sh
+++ test.sh
@@ -1,29 +1,64 @@
 #!/usr/bin/env bash
 
-declare __description __command
-declare -A __args=()
-declare -A __flags=()
-declare argv="$*"
+{
+  # Public variables
+  # ----------------
+  declare flag # determines whether a flag is present
+  declare -a arg # holds the values of an argument
+}
 
-declare flag
-declare -a arg
+{
+  # Internal variables
+  # ------------------
+
+  # current handler properties
+  declare __handler_description __handler_command __parent_handler
+
+  # handlers
+  declare -a __handlers
+  declare -A __handlers_hashes=()
+
+  # lists of [command]: [handler-function/description]
+  declare -A __commands=()
+  declare -A __commands_descriptions=()
+
+  declare __argv="$*" __command __opts
+  {
+    # set command by removing options
+    __command="${__argv%% -*}"
+    if [ "${__command:0:1}" = "-" ]; then __command=; fi
+
+    __opts="${__argv:${#__command}}"
+  }
 
 
-function get_command() {
-  local command="${*%% -*}"
-  if [ "${command:0:1}" != "-" ]; then echo "$command"; fi
+  # lists of [command]: [args/flags]
+  declare -A __args=()
+  declare -A __flags=()
 }
 
 
-command=$(get_command "$argv")
-opts="${argv:${#command}}"
+function __main() {
+  cat <<EOF
+Main function started
+
+command: $__command
+EOF
+
+  handler="${__commands["$__command"]}"
+  $handler && execute
+  declare -p __flags
+}
+
+
 
 function description() {
-  __description="$*"
+  __handler_description="$*"
 }
 
 function command() {
-  __command="$*"
+  __handler_command="$*"
+  [ -n "$__parent_handler" ] && __handler_command="${__parent_handler} $*"
 }
 
 # Registers an argument and sets $arg to its value, if present.
@@ -36,7 +71,8 @@ function command() {
 #   $3: parameter name
 #   $4: description
 function arg() {
-  local short; local long; local param; local desc;
+  local short long param desc i value;
+  local registered_args="${__args["${__handler_command}"]}"
   local args=( "$@" )
 
   for i in $(seq "$#" 1); do
@@ -53,28 +89,28 @@ function arg() {
     fi
   done
 
-  local args_list="${__args["${__command}"]}"
-  if [ -n "${args_list}" ]; then args_list+="\n"; fi
-  args_list+="${short:--}#${long:--}#${param}#${desc}"
+  if [ -n "${registered_args}" ]; then registered_args+="\n"; fi
+  registered_args+="${short:--}#${long:--}#${param}#${desc}"
 
-  __args+=( ["${__command:-"_"}"]="${args_list}")
+  # Global args are saved in _
+  __args+=( ["${__handler_command:-"_"}"]="${registered_args}")
   get_arg "$short" "$long"
 }
 
 
 
-# Retrieves an argument and sets $arg to its value
+# Retrieves an argument and sets $arg to its values
 function get_arg() {
-  local short="${1}"; local long="${2}"; local match=false;
-  local success=false
+  local short="${1}" long="${2}"
+  local match=false success=false
 
-  arg=()
+  arg=() # reset current value
 
-  for opt in $opts; do
+  for opt in $__opts; do
     if $match; then
       arg+=("$opt")
-      match=false
       success=true
+      match=false
       continue
     fi
 
@@ -95,7 +131,8 @@ function get_arg() {
 #   $1-$2: option (short and long)
 #   $3: description
 function flag() {
-  local short; local long; local desc;
+  local short long desc
+  local registered_flags="${__flags["${__handler_command}"]}"
 
   while [ $# -gt 1 ]; do
     local value="$1"
@@ -109,27 +146,26 @@ function flag() {
 
   desc="$1"
 
-  local flags_list="${__flags["${__command}"]}"
-  if [ -n "${flags_list}" ]; then flags_list+="\n"; fi
-  flags_list+="${short:--}#${long:--}#${desc}"
+  if [ -n "${registered_flags}" ]; then registered_flags+="\n"; fi
+  registered_flags+="${short:--}#${long:--}#${desc}"
 
-  __flags+=( ["${__command:-"_"}"]="${flags_list}" )
-
+  # Global flags are saved in _
+  __flags+=( ["${__handler_command:-"_"}"]="${registered_flags}" )
   has_flag "$short" "$long"
 }
 
-# Checks whether a flag is present
+# Checks whether a flag is present and sets $flag
 function has_flag() {
-  local short="${1}"; local long="${2}";
+  local short="${1}" long="${2}"
 
-  opt=false
+  flag=false
 
-  if [ -n "$long" ] && echo " ${opts} " | grep -qe " --${long} "; then
-    opt=true; return 0
+  if [ -n "$long" ] && echo " ${__opts} " | grep -qe " --${long} "; then
+    flag=true; return 0
   fi
 
-  if [ -n "$short" ] && echo " ${opts}" | grep -E " -[a-zA-Z0-9]*${short}"; then
-    opt=true; return 0
+  if [ -n "$short" ] && echo " ${__opts}" | grep -qE " -[a-zA-Z0-9]*${short}"; then
+    flag=true; return 0
   fi
 
   return 1
@@ -149,8 +185,9 @@ function @foo() {
   }
 
   function @foobar() {
-    echo "-- foo bar"
     command "foo"
+    echo "-- foo bar"
+    flag "g" "gggg"
     description "says goodbye"
 
     execute() {
@@ -178,36 +215,31 @@ function @bar() {
 
 
 
-
-declare -a handlers
-declare -A handlers_hashes=()
-
-declare -A commands=()
-declare -A commands_descriptions=()
-declare -A commands_args=()
-
 function handler_fingerprint() {
-  local handler="$1"
+  local handler="$1" hash
+
   hash=$(declare -f "@${handler}" | md5)
 
-  if [ -n "${handlers_hashes[$handler]}" ] && [ "${handlers_hashes[$handler]}" != "${hash}" ]; then
+  if [ -n "${__handlers_hashes[$handler]}" ] && [ "${__handlers_hashes[$handler]}" != "${hash}" ]; then
     echo "ERR ! Overwriting: $handler"
     exit 1
   fi
 
-  handlers_hashes+=( ["${handler}"]="${hash}" )
+  __handlers_hashes+=( ["${handler}"]="${hash}" )
 }
 
 function parse_handler() {
-  local prefix="${1:-}"; local new_handlers=()
-  local command="" description=""
+  local command old_parent_handler
+  local new_handlers=()
 
-  echo "Prefix: $prefix"
+  # Set top handler
+  old_parent_handler="$__parent_handler"
+  __parent_handler="${1:-}"
 
   # Read handlers
-  mapfile -t handlers < <( declare -F | grep '@' | cut -d'@' -f2 )
-  for handler in "${handlers[@]}"; do
-    if [ -z "${handlers_hashes["${handler}"]}" ]; then
+  mapfile -t __handlers < <( declare -F | grep '@' | cut -d'@' -f2 )
+  for handler in "${__handlers[@]}"; do
+    if [ -z "${__handlers_hashes["${handler}"]}" ]; then
       new_handlers+=( "${handler}" )
     fi
 
@@ -217,51 +249,34 @@ function parse_handler() {
   if [ "${#new_handlers[*]}" -eq 0 ]; then return 0; fi
 
   for handler in "${new_handlers[@]}"; do
-    __command="${handler}"
-    __description=""
+    __handler_command="${handler}"
+    __handler_description=""
 
-    ${handler/#/@}
-    __command="${prefix}${__command}" # prefixed nested command
+    ${handler/#/@} # evaluate handler function
 
+    # fail if a handler has no description
+    [ -z "${__handler_description}" ] && echo "ERR ! No description for ${handler}!" && exit 1
 
-    [ -z "${__description}" ] && echo "No description for ${handler}!" && exit 1
-
-    echo "command: $__command"
-    if [ -n "${commands["${__command}"]}" ]; then
-      echo "ERR! Cannot overwrite ${__command}"
+    if [ -n "${__commands["${__handler_command}"]}" ]; then
+      echo "ERR! Cannot overwrite ${__handler_command}"
       exit 1
     fi
 
-    commands+=( ["${__command}"]="@${handler}" )
-    commands_descriptions+=( ["${__command}"]="${__description}" )
+    __commands+=( ["${__handler_command}"]="@${handler}" )
+    __commands_descriptions+=( ["${__handler_command}"]="${__handler_description}" )
   done
 
   # Add recursion via function
-  parse_handler "${prefix}${handler} "
+  parse_handler "${handler}"
+  __parent_handler="${old_parent_handler}"
 }
 
-parse_handler ""
-readonly commands
-
-echo "Commands: ${commands[*]}"
-echo "Descriptions: ${commands_descriptions[*]}"
-echo
-echo
-
-
-echo "Args:"
-echo -e "${__args[*]}"
-echo "Global flags:"
-echo -e "${__flags["_"]}"
-echo "Command bar:"
-echo -e "${__flags["foo"]}"
-
+parse_handler
 
 currentArg=""
 
 function validate_short() {
-  local option=$1 command=$2
-  echo "Validating:"
+  local option="$1" command="$2"
 
   local flags="${__flags["_"]}\n${__flags["$command"]}"
   local args="${__args["_"]}\n${__args["$command"]}"
@@ -283,13 +298,13 @@ function validate_short() {
     fi
   done
 
-  echo "Unknown option: $1" && exit 1
+  echo "ERR ! Unknown option: $1" && exit 1
 }
 
-for opt in $opts; do
+for opt in $__opts; do
   if [ -n "$currentArg" ]; then
     if [ "${opt:0:1}" = "-" ]; then # value must not start with dash!
-      echo "value for $currentArg must not start with '-'" && exit 1
+      echo "ERR ! value for $currentArg must not start with '-'" && exit 1
     fi
 
     currentArg="";
@@ -297,14 +312,16 @@ for opt in $opts; do
   fi
 
   if [ "${opt:0:2}" = "--" ]; then
-    validate_short "${opt:2}" "$command"
+    validate_short "${opt:2}" "$__command"
   elif [ "${opt:0:1}" = "-" ]; then # validate short options as lists
     for (( i=1; i<${#opt}; i++ )); do
-      validate_short "${opt:$i:1}" "$command"
+      validate_short "${opt:$i:1}" "$__command"
     done
   fi
 done
 
 if [ -n "$currentArg" ]; then
-  echo "No value provided for $currentArg!" && exit 1
+  echo "ERR ! No value provided for $currentArg!" && exit 1
 fi
+
+__main "$@"
